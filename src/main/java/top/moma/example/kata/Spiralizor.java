1d518b3dc74dacb69875fb4e15c4d9a171f21913
jipeng.dong <jipeng.dong@opay-inc.com>
2022-10-17 11:16:30 +0800

    add all

--- src/main/java/top/moma/example/kata/Spiralizor.java
+++ src/main/java/top/moma/example/kata/Spiralizor.java
@@ -7,14 +7,36 @@ package top.moma.example.kata;
  *
  * <p>For example, spiral with size 5 should look like this:
  *
- * <p>00000 ....0 000.0 0...0 00000 and with the size 10:
+ * 00000
+ * ....0
+ * 000.0
+ * 0...0
+ * 00000
  *
- * <p>0000000000 .........0 00000000.0 0......0.0 0.0000.0.0 0.0..0.0.0 0.0....0.0 0.000000.0
- * 0........0 0000000000 Return value should contain array of arrays, of 0 and 1, with the first row
+ * <p>and with the size 10:
+ *
+ * 0000000000
+ * .........0
+ * 00000000.0
+ * 0......0.0
+ * 0.0000.0.0
+ * 0.0..0.0.0
+ * 0.0....0.0
+ * 0.000000.0
+ * 0........0
+ * 0000000000
+ *
+ * <p>Return value should contain array of arrays, of 0 and 1, with the first row
  * being composed of 1s. For example for given size 5 result should be:
  *
- * <p>[[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]] Because of the edge-cases for
- * tiny spirals, the size will be at least 5.
+ * <p>[
+ * [1,1,1,1,1],
+ * [0,0,0,0,1],
+ * [1,1,1,0,1],
+ * [1,0,0,0,1],
+ * [1,1,1,1,1]]
+ *
+ * <p>Because of the edge-cases for tiny spirals, the size will be at least 5.
  *
  * <p>General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.
  *
